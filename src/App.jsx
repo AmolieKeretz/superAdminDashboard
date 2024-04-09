e425c95089937492ef1f8222fd6338102b9c89cb
AmolieKeretz <fannymuneco25@gmail.com>
2024-04-09 15:35:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Theme
+import { themeSettings } from "./theme";
+
+// Scenes
+import {
+  Layout,
+  Dashboard,
+  Products,
+  Customers,
+  Transactions,
+  Monthly,
+  Admin,
+  Performance,
+  Orders,
+  Registration,
+  Items,
+} from "./scenes";
+
+// App
+const App = () => {
+  // Dark/Light mode
+  const mode = useSelector((state) => state.global.mode);
+  // theme setting
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        {/* Theme Provider */}
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            {/* Routes */}
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/monthly" element={<Monthly />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/performance" element={<Performance />} />   
+              <Route path="/registration" element={<Registration />} />             
+              <Route path="/orders/:id" element={<Items />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/scenes/index.js
@@ -0,0 +1,29 @@
+// Contains scenes to be exported in other files
+// ! Don't remove anything from here
+
+import Layout from "./layout";
+import Dashboard from "./dashboard";
+import Products from "./products";
+import Orders from "./orders";
+import Customers from "./customers";
+import Transactions from "./transactions";
+import Monthly from "./monthly";
+import Admin from "./admin";
+import Performance from "./performance";
+import Registration from "./registration";
+import Items from "./items";
+
+// export scenes
+export {
+  Layout,
+  Dashboard,
+  Products,
+  Orders,
+  Customers,
+  Transactions,
+  Monthly,
+  Admin,
+  Performance,
+  Registration,
+  Items,
+};
